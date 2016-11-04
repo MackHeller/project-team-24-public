8e51a34e72f487e14dd0b36a74305b24c4ad5eb7
mariabianchi3 <mariabianchi1995@gmail.com>
2016-11-04 16:29:25 -0400

    added drag feature for gates

---
+++ assets/scripts/DragHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+
+	public Rigidbody prefab;
+	Vector3 screenpoint;
+	Vector3 offset;
+	Rigidbody draggable;
+
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag (PointerEventData evenStData)
+	{
+		draggable = Instantiate(prefab);
+	}
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		float disttoscreen = Camera.main.WorldToScreenPoint(draggable.transform.position).z;
+		Vector3 posmove = Camera.main.ScreenToWorldPoint (
+			new Vector3 (Input.mousePosition.x, Input.mousePosition.y, disttoscreen));
+		draggable.transform.position = new Vector3 (posmove.x, transform.position.y, posmove.z);
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		//throw new System.NotImplementedException ();
+	}
+
+	#endregion
+}
+
+
