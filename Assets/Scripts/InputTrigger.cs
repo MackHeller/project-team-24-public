8f0f4a2d13c00d1fd659ead416dfccc897a7b354
hermankailey <hermankailey@mail.utoronto.ca>
2016-11-05 17:36:38 -0400

    Added a wire placeholder and input/output colliders

---
+++ Assets/Scripts/InputTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTrigger : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other){
+		Debug.Log ("Input Detected");
+	}
+	void OnTriggerExit (Collider other){
+		Debug.Log ("Input has been disconnected");
+	}
+}
---
+++ Assets/Scripts/OutputTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutputTrigger : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other){
+		Debug.Log ("Output Detected");
+	}
+	void OnTriggerExit (Collider other){
+		Debug.Log ("Output has been disconnected");
+	}
+}
