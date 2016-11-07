3f3a9d04d50759c5dbe223f26b4874a6d0b36f22
MackHeller <mack.heller@mail.utoronto.ca>
2016-11-07 13:53:44 -0500

    added setter methods, improved commenting

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -10,12 +10,12 @@ public class Level : MonoBehaviour{
 	 * set of LogicObjects, it expects a set of outputs
 	 * and compares whether the set of outputs is correct.
 	 */
-    protected String levelName;
-    protected int levelPar;
-    protected int minScore;
-    protected ArrayList<Module> gates;
-    protected ArrayList<bool> input;
-    protected ArrayList<bool> output;
+    private String levelName;
+    private int levelPar;
+    private int minScore;
+    private ArrayList<Module> gates;
+    private ArrayList<bool> input;
+    private ArrayList<bool> output;
     //for testing only
     void Start()
     {
@@ -27,10 +27,16 @@ public class Level : MonoBehaviour{
         this.levelName = levelName;
         setLevelValues(LevelReader.loadNewLevel(levelName));
     }
+    /*
+     * save level with a new file name
+     * */
     public void saveAsNewLevel(string levelName)
     {
         LevelWriter.saveAsNewLevel(levelName, this);
     }
+    /*
+     * save level with level's existing file name
+     * */
     public void saveAsNewLevel()
     {
         LevelWriter.saveAsNewLevel(this.levelName, this);
@@ -76,10 +82,20 @@ public class Level : MonoBehaviour{
         throw new System.
             Exception("Tried to request gate that does not exist in the current level");
     }
+    /* getter methods
+     */
     public ArrayList<Module> getGates() { return gates; }
     public ArrayList<bool> getLevelInput(){return input;}
     public ArrayList<bool> getLevelOutput(){return output;}
     public String getLevelName() { return levelName;}
     public int getLevelPar() { return levelPar; }
     public int getMinScore() { return minScore; }
+    /* setter methods
+     */
+    public void setGates(ArrayList<Module> gates) { this.gates= gates; }
+    public void setLevelInput(ArrayList<bool> input) { this.input = input; }
+    public void setLevelOutput(ArrayList<bool> output) { this.output = output; }
+    public void setLevelName(String levelName) { this.levelName = levelName; }
+    public void setLevelPar(int levelPar) { this.levelPar = levelPar; }
+    public void setMinScore(int minScore) { this.minScore = minScore; }
 }
