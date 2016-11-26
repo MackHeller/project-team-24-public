221cabd4d92fe7484c6aaf34a9de578d896c5bdb
hermankailey <hermankailey@mail.utoronto.ca>
2016-11-25 19:59:54 -0500

    Collision handling and gate deletion
    wires still not deletable, also trashcan sprite is just temporarily a
    red notgate.

---
+++ Assets/Scripts/DeletionTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeletionTrigger : MonoBehaviour {
+
+	public void OnTriggerStay2D(Collider2D other){
+		if (other.gameObject.CompareTag("justdropped")) {
+			Destroy (other.gameObject, 0.1f);
+			//when adding limited gates
+			//upon deletion the system should refund the gate
+		}
+	}
+
+}
