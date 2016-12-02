52c65ed486011ae943c204c84f47e42c395f2b9a
mariabianchi3 <mariabianchi1995@gmail.com>
2016-12-02 16:13:27 -0500

    main menu

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject button;
+	void Start () {
+		string[] levels = LevelReader.getAllLevelNames ();
+		Vector3 pos = this.transform.position;
+		foreach (string s in levels) {
+			pos.y = pos.y - 40;
+			GameObject pref = (GameObject) Instantiate (button, pos ,this.transform.rotation, this.transform);
+			pref.GetComponentInChildren<Text>().text = s;
+			Button b = pref.GetComponent<Button> ();
+
+		}
+	}
+
+}
