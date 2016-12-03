01f35ca32f4661dedf01ed990923ba005d164922
Freddy Zheng <freddy.zhengs@gmail.com>
2016-12-02 21:58:52 -0500

    added basic level concepts

---
+++ Assets/Scripts/LevelScripts/Levels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class Levels
+	{
+		public Levels ()
+		{
+		}
+	}
+}
+
---
+++ Assets/Scripts/LevelScripts/PremadeLevels.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Takes the name of a level, makes a filepath to that level and calls loadNewLevel
+/// If you already have the full filepath you can call Reader directly
+/// levelName = name of a valid level - the levelCreator
+/// </summary>
+public class PremadeLevels {
+
+	/// <summary>
+	/// Create an XOR gate.
+	/// Available gates: 	OR, NOT
+	/// Difficulty: 		1/5
+	/// </summary>
+	public void loadXorGatePuzzle() {
+
+	}
+
+	/// <summary>
+	/// Create a 4-input AND gate with only 2-input and gates.
+	/// Available gates: 	AND
+	/// Difficulty: 		1/5
+	/// </summary>
+	public void loadFourInputAndGate() {
+		
+	}
+}
