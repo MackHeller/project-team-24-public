f560ac28b72403b5570b0837ee7b8cd0a24058f8
Freddy Zheng <freddy.zhengs@gmail.com>
2016-12-02 23:03:11 -0500

    added level loading and input/output loading

--- Assets/Scripts/LevelScripts/PremadeLevels.cs
+++ Assets/Scripts/LevelScripts/PremadeLevels.cs
@@ -2,25 +2,53 @@
 using System.Collections;
 
 /// <summary>
-/// Takes the name of a level, makes a filepath to that level and calls loadNewLevel
-/// If you already have the full filepath you can call Reader directly
-/// levelName = name of a valid level - the levelCreator
+/// Class for loading premade levels.
 /// </summary>
-public class PremadeLevels {
+public class PremadeLevels : MonoBehaviour{
+
+	public GameObject inputPrefab;
+	public GameObject outputPrefab;
+	public GameObject canvas;
+	private float canvasWidth;
+	private float canvasHeight;
+
+	// the bottom-left x, y, z coordinates of the canvas shown on screen
+	private Vector3 canvasStart;
+
+	void Start() {
+		canvasWidth = canvas.GetComponent<RectTransform> ().rect.width;
+		canvasHeight = canvas.GetComponent<RectTransform> ().rect.height;
+		Vector3 canvasPos = canvas.transform.position;
+		canvasStart = new Vector3 (canvasPos.x - canvasWidth / 2, canvasPos.y - canvasHeight / 2, 0);
+
+		this.loadFourInputAndGate ();
+	}
+
+	private GameObject instantiateInputAt (float x, float y) {
+		GameObject newPrefab = Instantiate (inputPrefab);
+		newPrefab.transform.position = new Vector3 (canvasStart.x + x, canvasStart.y + y, 0);
+		return newPrefab;
+	}
+
+	private GameObject instantiateOutputAt (float x, float y) {
+		GameObject newPrefab = Instantiate (outputPrefab);
+		newPrefab.transform.position = new Vector3 (canvasStart.x + x, canvasStart.y + y, 0);
+		return newPrefab;
+	}
 
 	/// <summary>
 	/// Create an XOR gate.
 	/// Available gates: 	OR, NOT
-	/// Difficulty: 		1/5
+	/// Difficulty: 		1
 	/// </summary>
 	public void loadXorGatePuzzle() {
-
+		
 	}
 
 	/// <summary>
 	/// Create a 4-input AND gate with only 2-input and gates.
 	/// Available gates: 	AND
-	/// Difficulty: 		1/5
+	/// Difficulty: 		1
 	/// </summary>
 	public void loadFourInputAndGate() {
 		
