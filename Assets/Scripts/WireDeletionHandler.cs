6799730ef8fdda818e757e48074d36869d16bd0f
hermankailey <hermankailey@mail.utoronto.ca>
2016-12-03 03:38:40 -0500

    Added simple onclick wire deletion
    It deletes the gameobject but not the backend wire.

---
+++ Assets/Scripts/WireDeletionHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WireDeletionHandler : MonoBehaviour {
+	/// <summary>
+	/// This class detects a mouse click and deletes
+	/// the associated wire.
+	/// </summary>
+
+	private bool markedfordeletion = false;
+
+	void OnMouseDown(){
+		markedfordeletion = true;
+	}
+
+	void OnMouseUp(){
+		if (markedfordeletion) {
+			Destroy (gameObject, 0.1f);
+			//removes the gameobject
+			//but the backend wire remains
+			//TODO:need to remove wire from junction
+		}
+	}
+}
