1415c6fbc0c9f20eaa770e20f9919d3bde9453a3
MackHeller <mack.heller@mail.utoronto.ca>
2016-12-05 00:59:50 -0500

    solution serialization

---
+++ Assets/LevelScripts/Solution.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using C5;
+
+public class Solution {
+    private ArrayList<int> inputs;
+    private ArrayList<int> outputs;
+    private ArrayList<ArrayList<bool>> listOfInputSolutions;
+    private ArrayList<ArrayList<bool>> listOfOutputSolutions;
+
+    void start(Level level)
+    {
+        inputs = new ArrayList<int>();
+        outputs = new ArrayList<int>();
+        listOfInputSolutions = new ArrayList<ArrayList<bool>>();
+        listOfOutputSolutions = new ArrayList<ArrayList<bool>>();
+    }
+    public void saveSolution(Level level)
+    {
+        SolutionWriter.
+    }
+
+    public void setInputsAndOutputs(ArrayList<int> inputs, ArrayList<int> outputs)
+    {
+        this.inputs = inputs;
+        this.outputs = outputs;
+    }
+
+    public void addARowToSolution(ArrayList<bool> inputSolution, ArrayList<bool> outputSolution)
+    {
+        //if the amount of bools equals the amount of input and outputs correctly
+        if(inputSolution.Count == inputs.Count && outputSolution.Count == outputs.Count)
+        {
+            listOfInputSolutions.Add(inputSolution);
+            listOfOutputSolutions.Add(outputSolution);
+        }
+    }
+    public ArrayList<int> getInput() { return inputs; }
+    public ArrayList<int> getOutput() { return outputs; }
+    public ArrayList<ArrayList<bool>> getInputSolutions() { return listOfInputSolutions; }
+    public ArrayList<ArrayList<bool>> getOutputSolutions() { return listOfOutputSolutions; }
+}
---
+++ Assets/LevelScripts/SolutionWriter.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+using System;
+using System.IO;
+using C5;
+
+public class SolutionWriter
+{
+
+    public static void saveAsNewSolution(Level level, Solution solution)
+    {
+        writerSolution(Application.dataPath + "/Solutions/" + level.getFileName() + "-solution.JSON", level, solution);
+    }
+
+    private static void writerSolution(string filePath, Level level, Solution solution)
+    {
+        StreamWriter sw = null;
+        string json = "{\"LevelName\": \"" + level.getLevelName() + "\",\n\"Creator\": \"" + level.getCreator();
+
+        //do inputs
+        ArrayList<Int32> inputs = solution.getInput();
+        if (inputs.Count > 0)
+            json = json + ",\n\"Inputs\": [";
+        for (int i = 0; i < inputs.Count; i++)
+        {
+            json = json + "{\"Id\": " + inputs[i] + "},\n";
+        }
+        if (inputs.Count > 0)
+            json = json.Remove(json.Length - 2) + "\n]";
+
+        //do outputs
+        ArrayList<Int32> outputs = solution.getOutput();
+        if (outputs.Count > 0)
+            json = json + ",\n\"Outputs\": [";
+        for (int i = 0; i < outputs.Count; i++)
+        {
+            json = json + "{\"Id\": " + outputs[i] + "},\n";
+        }
+        if (outputs.Count > 0)
+            json = json.Remove(json.Length - 2) + "\n]";
+        json = json + "}";
+
+        ArrayList<ArrayList<bool>> listOfInputSolutions = solution.getInputSolutions();
+        ArrayList<ArrayList<bool>> listOfOutputSolutions = solution.getOutputSolutions();
+        for (int j=0;j< listOfInputSolutions.Count;j++) {
+            //do inputs
+            ArrayList<bool> inputSolution = listOfInputSolutions[j];
+            if (inputSolution.Count > 0)
+                json = json + ",\n\"InputSolution\": [";
+            for (int i = 0; i < inputSolution.Count; i++)
+            {
+                json = json + "{\"Value\": " + inputSolution[i] + "},\n";
+            }
+            if (inputSolution.Count > 0)
+                json = json.Remove(json.Length - 2) + "\n]";
+
+            //do outputs
+            ArrayList<bool> outputSolution = listOfOutputSolutions[j];
+            if (outputSolution.Count > 0)
+                json = json + ",\n\"OutputSolution\": [";
+            for (int i = 0; i < outputSolution.Count; i++)
+            {
+                json = json + "{\"Value\": " + outputSolution[i] + "},\n";
+            }
+            if (outputSolution.Count > 0)
+                json = json.Remove(json.Length - 2) + "\n]";
+            json = json + "}";
+        }
+        try
+        {
+            sw = new StreamWriter(filePath);
+            sw.WriteLine(json);
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (sw != null)
+                sw.Dispose();
+        }
+    }
+}
